880fb1df6e81f90341596ff43316a65cfb23384d
Shamsuzzoha <120673719+Shamsuzzoha1@users.noreply.github.com>
2022-12-22 10:47:33 +0530

    Remove Confid

--- app/src/main/java/com/apicall/weatherapp/model/Constant.java
+++ app/src/main/java/com/apicall/weatherapp/model/Constant.java
@@ -2,6 +2,6 @@ package com.apicall.weatherapp.model;
 
 public class Constant {
     public static final String baseUrl = "https://api.weatherapi.com/v1/";
-    public static final String apiKey = "d481aaf57c954ae1a1a45437221412";
+    public static final String apiKey = "ENTER YOUR WEATHER API KEY, Go to https://www.weatherapi.com/";
     public static final String LOG_TAG = "sizdebug";
 }
